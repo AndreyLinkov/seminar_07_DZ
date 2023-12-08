fb5172d656f0f5e110d54f6cb656df5ba39e3530
Andrey Linkov <linkov.developer@inbox.ru>
2023-12-08 05:03:13 +0300

    add task_04

---
+++ task_04/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задайте строку, состоящую из слов, разделенных пробелами. 
+// Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+
+string str = "Hello my world";
+string[] words = str.Split(' ');
+Array.Reverse(words);
+string result = string.Join(" ", words);
+
+Console.WriteLine(result);
