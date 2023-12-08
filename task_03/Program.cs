ad4f88c650ad371e7b9187fc5debc7decfd4504d
Andrey Linkov <linkov.developer@inbox.ru>
2023-12-08 04:57:11 +0300

    add task_03

---
+++ task_03/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string str = "Шалаш".ToLower();
+char[] array = str.ToCharArray();
+Array.Reverse(array);
+string newStr = new string(array);
+
+if (str == newStr)
+    Console.WriteLine($"Строка '{str}' является палиндромом");
+else
+    Console.WriteLine($"Строка '{str}' не является палиндромом");
